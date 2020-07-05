4dca7e4505da61c31ea53f99096c68ce189649ef
Sebastian Otalora <jhonsebas77@outlook.com>
2020-07-05 11:54:56 -0500

    Can navigate between scenes

---
+++ Dots_Game/Assets/Home_Screen/Scripts/Btn_Home_Screen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Btn_Home_Screen : MonoBehaviour
+{
+    public void goToNextScreen()
+    {
+        SceneManager.LoadScene("Play");
+    }
+}
