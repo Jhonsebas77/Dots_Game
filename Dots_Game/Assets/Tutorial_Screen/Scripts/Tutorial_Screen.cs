59774bce35b578d3c89473c988ba729310caa1f3
Sebastian Otalora <jhonsebas77@outlook.com>
2020-07-20 21:50:57 -0500

    Fix screen resize for two scenes

--- Dots_Game/Assets/Tutorial_Screen/Scripts/Tutorial_Screen.cs
+++ Dots_Game/Assets/Tutorial_Screen/Scripts/Tutorial_Screen.cs
@@ -8,6 +8,12 @@ public class Tutorial_Screen : MonoBehaviour
     public GameObject Step_01;
     public GameObject Step_02;
     public GameObject Step_03;
+      void Start()
+    {
+        Step_01.SetActive(true);
+        Step_02.SetActive(false);
+        Step_03.SetActive(false);
+    }
     public void goToBackScreen()
     {
         SceneManager.LoadScene("02_Play");
