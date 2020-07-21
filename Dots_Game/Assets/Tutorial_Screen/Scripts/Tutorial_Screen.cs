dbb3754a5c486ab39b99204b811c3874baee5d7b
Sebastian Otalora <jhonsebas77@outlook.com>
2020-07-20 20:41:50 -0500

    Add script to manage the instrucctions steps

---
+++ Dots_Game/Assets/Tutorial_Screen/Scripts/Tutorial_Screen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tutorial_Screen : MonoBehaviour
+{
+    public GameObject Step_01;
+    public GameObject Step_02;
+    public GameObject Step_03;
+    public void goToBackScreen()
+    {
+        SceneManager.LoadScene("02_Play");
+    }
+    public void goToStep01ToStep02()
+    {
+        Step_01.SetActive(false);
+        Step_02.SetActive(true);
+        Step_03.SetActive(false);
+    }
+    public void goToStep02ToStep03()
+    {
+        Step_01.SetActive(false);
+        Step_02.SetActive(false);
+        Step_03.SetActive(true);
+    }
+}
